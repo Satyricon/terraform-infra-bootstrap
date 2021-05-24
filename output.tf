93c69915eaecd6a745f8aef04080c1cb3417e68d
Yuri Barseghyan <yubarseg@gmail.com>
2021-05-24 22:45:51 +0300

    added more outputs (needed for the project pipelines setup)

--- output.tf
+++ output.tf
@@ -13,10 +13,24 @@ output "storage_account_state_container_name" {
   description = "Storage account container name"
 }
 
+output "tenant_id" {
+  value       = data.azurerm_subscription.current.tenant_id
+  description = "Tenant ID"
+}
+
+output "subscription_id" {
+  value       = data.azurerm_subscription.current.subscription_id
+  description = "Subscription ID"
+}
+
 output "user_assigned_managed_identity_object_id" {
   value = azurerm_user_assigned_identity.main.principal_id
 }
 
 output "terraform_subnet_id" {
   value = azurerm_subnet.main.id
+}
+
+output "pool_name" {
+  value = var.azure_dev_ops_agent_pool_name
 }
